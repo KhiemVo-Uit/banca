2caf001b6877720b7295bd5885741187ed4a0243
KhiemVo-Uit <24730039@ms.uit.edu.vn>
2025-05-09 10:58:48 +0700

    ok1

--- BANCA - Copy/src/main/java/com/example/BANCA/Repository/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.BANCA.Repository;
-
-import com.example.BANCA.Entyti.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
-}
--- BANCA - Copy/src/main/java/com/example/BANCA/Repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.BANCA.Repository;
-
-import com.example.BANCA.Entyti.Orders;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Orders,Long> {
-}
